87d642b8c9d6ca5f74789d29c02d0bf54804405f
srushti patel <127039741+srush953@users.noreply.github.com>
2025-04-20 16:57:15 +0530

    Update App.jsx

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -5,7 +5,7 @@ import Register from "./pages/Register";
 import Createblogs from "./pages/Createblogs";
 import EditBlog from "./pages/EditBlog";
 import Home from "./componenets/Home";
-import { ToastContainer } from 'react-toastify'
+
 import AddCategory from "./pages/AddCategory";
 import DisplayCategory from "./pages/DisplayCategory";
 
@@ -29,7 +29,7 @@ function App() {
 
 
       </Routes>
-      <ToastContainer />
+ 
 
     </div>
   );
